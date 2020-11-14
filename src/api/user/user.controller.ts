b25545f16e94e6d3681a196208f89c254f366447
vanessrw <vanessarebecca29@gmail.com>
2020-11-14 12:07:21 +0700

    w10

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,19 +1,46 @@
-
 import {Request, response, Response} from 'express';
+import userRouter from '.';
+import User from './user.model'
+
 
 export const create = async (req: Request, res: Response) => {
     console.log("req: ", req.body);
-    const body = req.body
+    let body = req.body;
+
+    try{
+        const response = await User.create(body);
+        return res.status(201).json({success: response});
+    }catch(err){
+        return res.status(500).json({message: err})
+    }
 }
 export const index = async (req: Request, res: Response) => {
     console.log("req: ", req.body);
-    const body = req.body
+    try{
+        let users = await User.find({});
+        return res.status(200).json({data: users});
+    }catch(err){
+        return res.status(500).json({message: err})
+    }
 }
 export const remove = async (req: Request, res: Response) => {
-    console.log("req: ", req.body);
-    const body = req.body
+    try{
+        let id = req.params.id;
+        let body = req.body;
+        let response = await User.findByIdAndDelete(id, body);
+        return res.status(200).json(response);
+    }catch(err){
+        return res.status(500).json({message: err})
+    }
 }
 export const update = async (req: Request, res: Response) => {
     console.log("req: ", req.body);
-    
+    try{
+        let id = req.params.id;
+        let body = req.body;
+        let response = await User.findByIdAndUpdate(id, body);
+        return res.status(200).json(response);
+    }catch(err){
+        return res.status(500).json({message: err})
+    }
 }
